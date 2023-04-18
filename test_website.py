e4591952644243f7b48a4a9b6bf8211d06e60669
sanjay122000 <120300027+sanjay122000@users.noreply.github.com>
2023-04-18 09:08:54 +0530

    first commit
    first commit

--- test_website.py
+++ test_website.py
@@ -1,16 +1,19 @@
 import unittest
 import requests
+
 class TestWebsite(unittest.TestCase):
-def test_website_loads(self):
-    response = requests.get("https://atg.world")
-    self.assertEqual(response.status_code, 200)
-def test_website_loads(self):
-    try:
-        response = requests.get("https://atg.world")
-        self.assertEqual(response.status_code, 200)
-    except Exception as e:
-        print("Error:", e)
-        self.fail("Website did not load properly")
+    def test_website_loads(self):
+        url = "https://atg.world"
+        response = requests.get(url)
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+        if status_code == 200:
+            print(f"INFO: Successfully loaded {url}")
+        else:
+            print(f"ERROR: Failed to load {url}")
+
 if __name__ == '__main__':
     unittest.main()
+
 
